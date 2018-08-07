657e15fbbaf4241fb371abd4772cd9e0efeec0f1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-07 11:27:04 -0700

    day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,6 +8,24 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot ABCD= new Robot();
+		ABCD.changeRobot("https://www.famousbirthdays.com/headshots/lil-pump-6.jpg");
+		ABCD.setSpeed(100);
+		ABCD.penDown();
+		int sides= 17;
+		int angle=360/sides;
+		for(int i=0;i<sides;i++) {
+			ABCD.move(100);
+			ABCD.turn(angle);
+			
+		}
+	
+	    ABCD.sparkle();
+		
+		
+		
+		
+	
 	
 		
 		
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,28 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot L=new Robot();
 		// 5. Set your robot's pen down 
-		
+				L.penDown();
 		// 3. Set the robot to go at max speed (100)
+		L.setSpeed(100);
 		
-		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		// 4. Use a for loop to repeat steps #7 to #8, 75
+		for(int i=0;i<74;i++){
+			
+		
 		
 			// 7. Change the pen color to random
+			L.setRandomPenColor();
 		
 			// 6. Move the robot 5 times the loop counter (5*i)
+			L.move(5*i);
 			
 			// 2. Turn the robot 360/7 degrees to the right
+			L.turn (360/7);
 		
 			// 8. Set the pen width to i
+		}
 			
 	}
 }
