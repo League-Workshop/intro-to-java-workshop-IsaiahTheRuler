3e3770cc365eadd3ddd2be22fa6d17a5eae9841d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-09 11:38:26 -0700

    day 4

---
+++ src/section3/HelloWorld.java
@@ -0,0 +1,17 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Lil pumps a goat");
+		String input=JOptionPane.showInputDialog("is lil pump a goat");
+		JOptionPane.showMessageDialog(null, input);
+		if(input.equalsIgnoreCase("Lil pumps a goat");
+				JOptionPane.showMessageDialog(null,"You are correct");
+				else {
+					JOptionPane.showMessageDialog(null,"u suk")
+				}
+	}
+`
+}
